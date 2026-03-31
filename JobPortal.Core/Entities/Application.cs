ba6134f2540a3a48a775663ce0ff112e003b0dda
Raginee Gupta <ragineegupta123@gmail.com>
2026-03-31 20:10:31 +0530

    Add project files.

---
+++ JobPortal.Core/Entities/Application.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Core.Entities
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int JobId { get; set; }
+        public string ResumePath { get; set; }
+        public string Status { get; set; }
+        public DateTime AppliedAt { get; set; }
+    }
+}
---
+++ JobPortal.Core/Entities/Job.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Core.Entities
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
